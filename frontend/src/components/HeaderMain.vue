5f767e9423863bfb7236d5f0153f3965d6bfc06b
Екатерина Лебедева <>
2022-02-18 00:18:49 +0300

    first commit

---
+++ frontend/src/components/HeaderMain.vue
@@ -0,0 +1,8 @@
+<template>
+    <header class="header-main">
+    </header>
+</template>
+
+<style scoped>
+    .header-main {}
+</style>
---
+++ frontend/src/main.js
@@ -0,0 +1,13 @@
+import 'normalize.css';
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import './styles/index.scss';
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
