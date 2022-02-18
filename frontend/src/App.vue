56fc2018d4738748d015ef66529799037a3bf5b9
Екатерина Лебедева <>
2022-02-18 08:47:57 +0300

    - Составлена цветовая схема сервиса - Описаны стили для различных видов кнопок

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -14,67 +14,68 @@ import HelloWorld from '@/components/HelloWorld.vue'
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
+
+      <table>
+          <thead>
+              <tr>
+                  <td>.button</td>
+                  <td>&lt;button&gt;</td>
+              </tr>
+          </thead>
+          <tbody>
+              <tr>
+                  <td>
+                    <div class="button">Пополнить баланс</div>
+                    <div class="button button--secondary">Пополнить баланс</div>
+                    <div class="button button--secondary button--disabled">Пополнить баланс</div>
+                  </td>
+                  <td>
+                    <button>Пополнить баланс</button>
+                    <button class="button--secondary">Пополнить баланс</button>
+                    <button class="button--secondary" disabled>Пополнить баланс</button>
+                  </td>
+              </tr>
+
+              <tr>
+                  <td>
+                    <div class="button button--height-md">Пополнить баланс</div>
+                    <div class="button button--height-md button--secondary">Пополнить баланс</div>
+                    <div class="button button--height-md button--secondary button--disabled">Пополнить баланс</div>
+                  </td>
+                  <td>
+                    <button class="button--height-md">Пополнить баланс</button>
+                    <button class="button--height-md button--secondary">Пополнить баланс</button>
+                    <button class="button--height-md button--secondary" disabled>Пополнить баланс</button>
+                  </td>
+              </tr>
+
+              <tr>
+                  <td>
+                    <div class="button button--height-lg">Пополнить баланс</div>
+                    <div class="button button--height-lg button--secondary">Пополнить баланс</div>
+                    <div class="button button--height-lg button--secondary button--disabled">Пополнить баланс</div>
+                  </td>
+                  <td>
+                    <button class="button--height-lg">Пополнить баланс</button>
+                    <button class="button--height-lg button--secondary">Пополнить баланс</button>
+                    <button class="button--height-lg button--secondary" disabled>Пополнить баланс</button>
+                  </td>
+              </tr>
+          </tbody>
+      </table>
     </div>
   </header>
 
   <RouterView />
 </template>
 
-<style>
-:root {
-  --vt-c-white: #ffffff;
-  --vt-c-white-soft: #f8f8f8;
-  --vt-c-white-mute: #f2f2f2;
-
-  --vt-c-black: #181818;
-  --vt-c-black-soft: #222222;
-  --vt-c-black-mute: #282828;
-
-  --vt-c-indigo: #2c3e50;
-
-  --vt-c-divider-light-1: rgba(60, 60, 60, 0.29);
-  --vt-c-divider-light-2: rgba(60, 60, 60, 0.12);
-  --vt-c-divider-dark-1: rgba(84, 84, 84, 0.65);
-  --vt-c-divider-dark-2: rgba(84, 84, 84, 0.48);
-
-  --vt-c-text-light-1: var(--vt-c-indigo);
-  --vt-c-text-light-2: rgba(60, 60, 60, 0.66);
-  --vt-c-text-dark-1: var(--vt-c-white);
-  --vt-c-text-dark-2: rgba(235, 235, 235, 0.64);
-}
-
-/* semantic color variables for this project */
-:root {
-  --color-background: var(--vt-c-white);
-  --color-background-soft: var(--vt-c-white-soft);
-  --color-background-mute: var(--vt-c-white-mute);
-
-  --color-border: var(--vt-c-divider-light-2);
-  --color-border-hover: var(--vt-c-divider-light-1);
-
-  --color-heading: var(--vt-c-text-light-1);
-  --color-text: var(--vt-c-text-light-1);
-
-  --section-gap: 160px;
-}
-
-@media (prefers-color-scheme: dark) {
-  :root {
-    --color-background: var(--vt-c-black);
-    --color-background-soft: var(--vt-c-black-soft);
-    --color-background-mute: var(--vt-c-black-mute);
-
-    --color-border: var(--vt-c-divider-dark-2);
-    --color-border-hover: var(--vt-c-divider-dark-1);
-
-    --color-heading: var(--vt-c-text-dark-1);
-    --color-text: var(--vt-c-text-dark-2);
-  }
-}
+<style lang="scss">
+@import '@/styles/palette.scss';
 
 html {
     font-family: 'Gilroy';
     font-weight: 400;
+    color: var(--text-color-regular);
 }
 
 *,
@@ -88,105 +89,16 @@ html {
 
 body {
   min-height: 100vh;
-  color: var(--color-text);
-  background: var(--color-background);
-  transition: color 0.5s, background-color 0.5s;
+  background: var(--background-color-0);
+  transition: color 0.5s,
+              background-color 0.5s;
   text-rendering: optimizeLegibility;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
 
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
-}
-
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
 .logo {
   display: block;
   margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
 }
 </style>
